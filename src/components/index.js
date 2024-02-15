ae91af5c228a458b0f5a96734bb0d696c1d0b5aa
estefabuitrago <125572826+estefabuitrago@users.noreply.github.com>
2024-02-15 11:15:47 -0500

    begin

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,15 @@
 import { CardsHome } from "./CardsHome";
+import MenuUser from "./MenuUser";
+import { Button } from "./Button";
+import { IconoMouse } from './IconoMouse'; 
+import { Card } from './Card'; 
+import { Footer } from './Footer'; 
 
 export {
-    CardsHome
+    CardsHome,
+    MenuUser,
+    Button,
+    IconoMouse, 
+    Card, 
+    Footer
 }
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,5 +1,14 @@
 import homeAdmin from './admin/home/style.css'
+import homeUser from './user/home/style.css'
+import menuUser from './components/menuUser/style.css'
+import card from './components/card/style.css'
+import modalCard from './components/card/modalCard.css'
+import buttonTitle from './components/buttonTitle/style.css'
 
 export {
-    homeAdmin
+    homeAdmin,
+    homeUser,
+    card,
+    modalCard,
+    buttonTitle
 }
